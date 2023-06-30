339503d0b9c974227f0ed04dfb31a26f32df071e
Jesús Manuel López Fabián <jesusmlf7@gmail.com>
2023-06-29 20:55:43 -0600

    110A

---
+++ CodeForces/ProblemSet/110A-NearlyLuckyNumber.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string num;
+    cin>>num;
+    map<char,int> bucket1;
+
+    for(int i=0; i<num.length(); i++){
+        if(num[i] == '4' || num[i] == '7'){
+            bucket1[num[i]] ++;
+        }
+    }
+
+    int suma = 0;
+    for(auto it = bucket1.begin(); it != bucket1.end(); it++){
+        suma += it->second;
+    }
+    cout<<suma<<endl;
+    string ans = "YES";
+
+    while(suma != 0){
+        if(suma % 10 == 4 || suma % 10 == 7){
+            ans = "NO";
+            break;
+        }
+        suma /= 10;
+    }
+    cout<<ans<<endl;
+    cout<<7 % 10;
+}
