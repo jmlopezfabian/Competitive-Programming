a7c0de7bfd1137c0dc3f8967181e4e305060bd85
jesus <jmlopezfabian@gmail.com>
2024-06-07 11:04:58 -0600

    Div2 939

---
+++ CodeForces/Contest/Div2-939/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+    return 0;
+}
